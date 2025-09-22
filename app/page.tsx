071c0d0dabd5c2df7fc6c8c786ea96846025943e
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-09-22 11:28:45 -0400

    setup shadcn ui and tested a button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <h1>Let&#39;s build AI productivity App</h1>
+      <Button>Click Me</Button>
     </main>
   )
 }
